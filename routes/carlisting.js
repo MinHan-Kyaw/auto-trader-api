f7ef1ef793bdbfe1dde0a84ea2d6d8514f2ba9c1
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-04-22 02:43:55 +0700

    [Add] car listing photos

--- routes/carlisting.js
+++ routes/carlisting.js
@@ -1,12 +1,20 @@
 var express = require("express");
+var router = express.Router();
+const multer = require("multer");
+const bodyParser = require("body-parser");
 const CarListingController = require("../controllers/CarListingController");
 
-var router = express.Router();
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
-router.post("/", CarListingController.addCarListing);
+// Configure body-parser middleware to parse text data
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post("/", upload.array("photos"), CarListingController.addCarListing);
 router.get("/", CarListingController.carlistingList);
 router.get("/:id", CarListingController.carListingDetail);
-router.put("/:id", CarListingController.updateCarListing);
+router.put("/:id", upload.array("photos"), CarListingController.updateCarListing);
 router.delete("/:id", CarListingController.deleteCarListing);
 
 module.exports = router;
