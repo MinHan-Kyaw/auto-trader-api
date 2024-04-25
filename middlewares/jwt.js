6d352fa221e0cba829fb6b81414d8d6c834bd060
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-04-25 15:29:48 +0700

    [Add] pagination in car listing and check token expire

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,8 +2,25 @@ var { expressjwt: jwt } = require("express-jwt");
 const secret = process.env.JWT_SECRET;
 
 const authenticate = jwt({
-    secret: secret,
-    algorithms: ["HS256"], // Specify the algorithm used to sign the token
+  secret: secret,
+  algorithms: ["HS256"], // Specify the algorithm used to sign the token
 });
 
-module.exports = authenticate;
+// module.exports = authenticate;
+module.exports = function (req, res, next) {
+  authenticate(req, res, function (err) {
+    if (err) {
+      console.error("JWT Error:", err.message);
+      return res.status(401).json({ message: "Unauthorized" });
+    } else {
+      if (!req.auth) {
+        return apiResponse.ErrorResponse(
+          res,
+          { message: "Token expired" },
+          401
+        );
+      }
+    }
+    next();
+  });
+};
